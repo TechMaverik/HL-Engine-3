1fe4d39ef225c2dbde035e0623817875cf0be4ef
TechMaverik <informakhiljacob@gmail.com>
2025-07-11 15:58:02 +0530

    datamodel

---
+++ datamodel/go2.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class Go2(BaseModel):
+    x: float
+    y: float 
+    z: float 
--- routers.py
+++ routers.py
@@ -1,6 +1,7 @@
 import uvicorn
 from constants import *
 from fastapi import FastAPI
+from datamodel.go2 import Go2
 from hlengine_services import Go2Services as go2
 
 engine = FastAPI()
@@ -14,10 +15,31 @@ def version():
 def go2_standup():
     go2().standup()
 
-
 @engine.get("/go2_standdown")
 def go2_standdown():
     go2().standdown()
+
+@engine.get("/go2_forward")
+def go2_forward():
+    go2().movement(1, 0, 0)
+
+@engine.get("/go2_left")
+def go2_left():
+    go2().movement(0, 1, 0)
+
+@engine.get("/go2_rotate")
+def go2_rotate():
+    go2().movement(0, 0, 1)
+
+@engine.get("/go2_move")
+def go2_move(Go2: Go2):
+    print(Go2.x, Go2.y, Go2.z)
+    go2().movement(Go2.x, Go2.y, Go2.z)
+
+@engine.get("/stop")
+def stop():
+    go2().stop()
+
 
 
 if __name__ == "__main__":
