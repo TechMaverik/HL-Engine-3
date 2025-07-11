26e7b6fce66d0ee7d34191081c29c0bd836e7171
TechMaverik <informakhiljacob@gmail.com>
2025-07-11 08:07:51 +0530

    updated routers

--- routers.py
+++ routers.py
@@ -1,7 +1,7 @@
 import uvicorn
 from constants import *
 from fastapi import FastAPI
-
+from hlengine_services import Go2Services as go2
 
 engine = FastAPI()
 
@@ -9,6 +9,15 @@ engine = FastAPI()
 @engine.get("/")
 def version():
     return VERSION
+
+@engine.get("/go2_standup")
+def go2_standup():
+    go2().standup()
+
+
+@engine.get("/go2_standdown")
+def go2_standdown():
+    go2().standdown()
 
 
 if __name__ == "__main__":
